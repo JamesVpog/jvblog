c23bd7b73b0e5e0d823f68fa8e0f6aef852d729c
James Vongphasouk <james.vongpha@gmail.com>
2024-10-13 21:00:10 -0600

    skeleton blog

---
+++ eleventy.config.js
@@ -0,0 +1,15 @@
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("./src/css");
+    eleventyConfig.addWatchTarget("./src/css");
+    return {
+        dir : {
+            input: 'src',
+            includes: '_includes',
+            output: '_site',
+        },
+        templateFormats: ['md', 'njk', 'html'],
+        markdownTemplateEngine: 'njk',
+        htmlTemplateEngine: 'njk',
+        dataTemplateEngine: 'njk'
+    };
+}
